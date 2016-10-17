c083d27ddfa68fbc7b9eb09fc4ebc9bc15df67b4
胡伟亚 <596031524@qq.com>
2016-10-17 11:04:20 +0800

    Initial Commit

---
+++ HUAVPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HUAVPlayer
+//
+//  Created by huweiya on 16/10/17.
+//  Copyright © 2016年 5i5j. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
